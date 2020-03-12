f98feedec9131afd9a10fa07a7f753e5a6dbdc9f
Herb Stahl <ghstahl@gmail.com>
2020-03-12 13:10:38 -0700

    linter

--- src/main.ts
+++ src/main.ts
@@ -25,43 +25,55 @@ async function run(): Promise<void> {
 
     core.setOutput('time', new Date().toTimeString())
     // Set user agent varable
-    const usrAgentRepo = crypto.createHash('sha256').update(`${process.env.GITHUB_REPOSITORY}`).digest('hex')
+    const hash = crypto.createHash('sha256')
+    const update = hash.update(`${process.env.GITHUB_REPOSITORY}`)
+    const usrAgentRepo = update.digest('hex')
     const actionName = 'AzureLogin'
-    const userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS_${actionName}_${usrAgentRepo}`
+
+    const bPrefix = !!prefix
+    const sPrefix = bPrefix ? `${prefix}+` : ''
+    const userAgentString = `${sPrefix}GITHUBACTIONS_${actionName}_${usrAgentRepo}`
     core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString)
     console.log(`userAgentString:${userAgentString}.`)
 
     azPath = await io.which('az', true)
     console.log(`azPath:${azPath}.`)
-    await executeAzCliCommand("--version")
+    await executeAzCliCommand('--version')
 
-    let creds = core.getInput('creds', { required: true })
+    const creds = core.getInput('creds', {required: true})
     console.log(`creds:${creds}.`)
 
-    let secrets = new SecretParser(creds, FormatType.JSON)
+    const secrets = new SecretParser(creds, FormatType.JSON)
 
-    let servicePrincipalId = secrets.getSecret("$.clientId", false)
-    let servicePrincipalKey = secrets.getSecret("$.clientSecret", true)
-    let tenantId = secrets.getSecret("$.tenantId", false)
-    let subscriptionId = secrets.getSecret("$.subscriptionId", false)
-    if (!servicePrincipalId || !servicePrincipalKey || !tenantId || !subscriptionId) {
-        throw new Error("Not all values are present in the creds object. Ensure clientId, clientSecret, tenantId and subscriptionId are supplied.")
+    const servicePrincipalId = secrets.getSecret('$.clientId', false)
+    const servicePrincipalKey = secrets.getSecret('$.clientSecret', true)
+    const tenantId = secrets.getSecret('$.tenantId', false)
+    const subscriptionId = secrets.getSecret('$.subscriptionId', false)
+    if (
+      !servicePrincipalId ||
+      !servicePrincipalKey ||
+      !tenantId ||
+      !subscriptionId
+    ) {
+      throw new Error(
+        'Not all values are present in the creds object. Ensure clientId, clientSecret, tenantId and subscriptionId are supplied.'
+      )
     }
 
-    await executeAzCliCommand(`login --service-principal -u "${servicePrincipalId}" -p "${servicePrincipalKey}" --tenant "${tenantId}"`)
+    await executeAzCliCommand(
+      `login --service-principal -u "${servicePrincipalId}" -p "${servicePrincipalKey}" --tenant "${tenantId}"`
+    )
     await executeAzCliCommand(`account set --subscription "${subscriptionId}"`)
-    console.log("Login successful.");    
-
+    console.log('Login successful.')
   } catch (error) {
     core.setFailed(error.message)
   }
 }
-async function executeAzCliCommand(command: string) {
+async function executeAzCliCommand(command: string): Promise<void> {
   try {
-      await exec.exec(`"${azPath}" ${command}`, [],  {}); 
-  }
-  catch(error) {
-      throw new Error(error);
+    await exec.exec(`"${azPath}" ${command}`, [], {})
+  } catch (error) {
+    throw new Error(error)
   }
 }
 
