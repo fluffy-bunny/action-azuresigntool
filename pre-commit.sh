30a184cc12461ffa7c7f6ec56b98ef8f253e6478
Herb Stahl <ghstahl@gmail.com>
2020-03-30 20:30:38 -0700

    test

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-​counter=0
-while read -r line
-do
+
+counter=0
+while read -r line; do
   (( counter+=1 ))
   echo "$counter: $line"
 done < <(git diff --name-only)
-if [ $counter -gt 0 ]
-then
-echo Hey that\'s a large number of files: $counter
-exit 0
+echo $counter
+
+if [[ $counter -gt 0 ]]; then
+  echo "Committing $counter files"
+  exit 0
 fi
+
 echo "nothing being committed"
 exit 1
