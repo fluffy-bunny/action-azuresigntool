95264722bcfd1f3beda76e9fc962d7f6b0b54f6d
Herb Stahl <ghstahl@gmail.com>
2020-03-30 20:02:09 -0700

    bump

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-​
-counter=0
-while read -r line 
+​counter=0
+while read -r line
 do
   (( counter+=1 ))
   echo "$counter: $line"
 done < <(git diff --name-only)
- 
 if [ $counter -gt 0 ]
 then
 echo Hey that\'s a large number of files: $counter
 exit 0
 fi
-
- 
 echo "nothing being committed"
 exit 1
-
